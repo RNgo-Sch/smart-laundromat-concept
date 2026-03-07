33df4f18cb37acb9caf29dab52e1e8815ae43a78
Jiahao <yaojiahao048@gmail.com>
2026-03-08 00:08:04 +0800

    got the sign in and sign up working v1

--- app/src/main/java/com/example/smart_laundromat_concept/page/MainActivity.java
+++ app/src/main/java/com/example/smart_laundromat_concept/page/MainActivity.java
@@ -1,22 +1,21 @@
 package com.example.smart_laundromat_concept.page;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.example.smart_laundromat_concept.R;
+
 
 public class MainActivity extends AppCompatActivity {
 
-
-    EditText username;
-    EditText password;
-    Button loginButton;
+    EditText activity_main__username_text;
+    EditText activity_main__password_text;
+    Button activity_main__login_Button;
+    Button activity_main__go_to_signup_Button;
 
 
     @Override
@@ -24,14 +23,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        username = findViewById(R.id.username);
-        password = findViewById(R.id.password);
-        loginButton = findViewById(R.id.loginButton);
+        activity_main__username_text = findViewById(R.id.activity_main__username_text);
+        activity_main__password_text = findViewById(R.id.activity_main__password_text);
+        activity_main__login_Button = findViewById(R.id.activity_main__login_Button);
+        activity_main__go_to_signup_Button = findViewById(R.id.activity_main__go_to_signup_Button);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        activity_main__login_Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                if (activity_main__username_text.getText().toString().equals("user") && activity_main__password_text.getText().toString().equals("1234")) {
                     Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
                     launchPage(view);
 
@@ -40,15 +40,52 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        activity_main__go_to_signup_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchPage(view);
+            }
+        });
     }
+
+//    public void login_verification(View view) {
+//        if (username_text_activity_main.getText().toString().equals("user") && password_text_activity_main.getText().toString().equals("1234")) {
+//            Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+//            launchPage(view);
+//
+//        } else {
+//            Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+//        }
+//
+//    }
+
     public void launchPage(View view) {
 
         Intent intent = null;
         int id = view.getId();
-        if (id == R.id.loginButton) {
+
+        //check which button was clicked
+
+        //====================================
+        // Buttons for activity_main.xml
+        //====================================
+        if (id == R.id.activity_main__login_Button) {
             intent = new Intent(this, HomePageActivity.class);
-        } else if (id == R.id.signupButton) {
+
+        } else if (id == R.id.activity_main__go_to_signup_Button) {
             intent = new Intent(this, SignUpActivity.class);
+
+
+        //====================================
+        // Buttons for activity_sign_up.xml
+        //====================================
+//        } else if (id == R.id.create_Account_Button_activity_sign_up) {
+//            intent = new Intent(this, MainActivity.class);
+//            Toast.makeText(MainActivity.this, "Account Created Successfully!", Toast.LENGTH_LONG).show();
+//
+//        } else if (id == R.id.return_to_Signin_Button_acitivity_sign_up) {
+//            intent = new Intent(this, MainActivity.class);
         }
 
         if (intent != null) {
--- app/src/main/java/com/example/smart_laundromat_concept/page/SignUpActivity.java
+++ app/src/main/java/com/example/smart_laundromat_concept/page/SignUpActivity.java
@@ -1,6 +1,11 @@
 package com.example.smart_laundromat_concept.page;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +17,64 @@ import com.example.smart_laundromat_concept.R;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    EditText activity_sign_up__Signup_username_text;
+    EditText activity_sign_up__Signup_password_text;
+    Button activity_sign_up__Create_Account_Button;
+    Button activity_sign_up__Return_to_Signin_Button;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        activity_sign_up__Signup_username_text = findViewById(R.id.activity_sign_up__Signup_username_text);
+        activity_sign_up__Signup_password_text = findViewById(R.id.activity_sign_up__Signup_password_text);
+        activity_sign_up__Create_Account_Button = findViewById(R.id.activity_sign_up__Create_Account_Button);
+        activity_sign_up__Return_to_Signin_Button = findViewById(R.id.activity_sign_up__Return_to_Signin_Button);
+
+        activity_sign_up__Create_Account_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (activity_sign_up__Signup_username_text.getText().toString().equals("user") && activity_sign_up__Signup_password_text.getText().toString().equals("1234")) {
+                    Toast.makeText(SignUpActivity.this, "User already exists", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Toast.makeText(SignUpActivity.this, "Account Created Successfully!", Toast.LENGTH_LONG).show();
+                    launchPage(view);
+                }
+            }
+        });
+
+        activity_sign_up__Return_to_Signin_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchPage(view);
+            }
         });
     }
+    public void launchPage(View view) {
+
+        Intent intent = null;
+        int id = view.getId();
+
+        //check which button was clicked
+
+        //====================================
+        // Buttons for activity_sign_up.xml
+        //====================================
+        if (id == R.id.activity_sign_up__Create_Account_Button) {
+            intent = new Intent(this, MainActivity.class);
+
+        } else if (id == R.id.activity_sign_up__Return_to_Signin_Button) {
+            intent = new Intent(this, MainActivity.class);
+        }
+
+        if (intent != null) {
+            startActivity(intent);
+        }
+    }
+
 }
