be18f37d8ffddb53b4eeeeb6b93795bc9658b757
Jiahao <yaojiahao048@gmail.com>
2026-03-11 00:03:12 +0800

    doing profile page v2 modify the liquid glass look on the profile page, menu bar and the login page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
 }
 
 dependencies {
@@ -40,8 +41,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation("com.qmdeve.liquidglass:core:1.0.3")
+
+
+
     implementation(libs.play.services.maps)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
